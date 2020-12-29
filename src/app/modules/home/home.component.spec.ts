0209a9eeb5ee0ebad898cdb5f886c4240f1d712f
Juan Antonio Moreno Valderrama <jamv999@gmail.com>
2020-12-29 19:58:32 +0100

    add: template home.component tests

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -7,10 +7,13 @@ import { TabViewModule } from 'primeng/tabview';
 
 import { HomeComponent } from './home.component';
 import { HomeService } from '../../services/home.service';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let el: DebugElement;
   let translateService: any;
 
   beforeEach(async () => {
@@ -29,8 +32,9 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
-    translateService = TestBed.inject(TranslateService);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
+    translateService = TestBed.inject(TranslateService);
     translateService.addLangs(['en', 'es', 'ar']);
     translateService.setDefaultLang('es');
     translateService.use('es');
@@ -42,8 +46,21 @@ describe('HomeComponent', () => {
   });
 
   it('change language', () => {
+    // This test is used to check the html template.
     translateService.use('en');
     fixture.detectChanges();
     expect(component).toBeTruthy();
+    // check gallery: in this case check the number of thumbnails, will be 8.
+    const thumbnails = el.queryAll(By.css('.p-galleria-thumbnail-item'));
+    expect(thumbnails).toBeTruthy('Could not find thumnbails');
+    expect(thumbnails.length).toBe(8, 'Incorrect of number of thumbnails');
+    // check the tabs
+    const tabs = el.queryAll(By.css('.p-tabview-title'));
+    expect(tabs).toBeTruthy('Could not find tabs title');
+    expect(tabs.length).toBe(3, 'Incorrect of number of tabs');
+    // check cards
+    const cards = el.queryAll(By.css('.p-card'));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(4, 'Incorrect of number of cards');
   });
 });
